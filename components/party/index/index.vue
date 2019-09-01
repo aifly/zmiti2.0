2036261428392acb62eedadee5568236f33d455d
bmyuan <bing-x.l.l@163.com>
2019-09-01 18:47:59 +0800

    add the partyList

--- components/party/index/index.vue
+++ components/party/index/index.vue
@@ -1,273 +1,416 @@
 <template>
-	<div class="zmiti-meetingroom-main-ui lt-full">
-		<Table style="margin:40px auto" width='800' height='300' :columns='dataSource.columns' :data='dataSource.data'></Table>
+	<div class="zmiti-party-main-ui">
+		<div class="zmiti-list-main">
+			<header class="zmiti-tab-header">
+				<div>活动列表</div>
+				<div>
+					<Button :loading='loading' @click="getDataList()" type="primary">刷新</Button>
+					<Button type="primary" @click='add()'>添加</Button>
+				</div>
+			</header>
+			<div class='zmiti-submit-main zmiti-scroll ' :style="{height:viewH - 130+'px' }">
+				<ZmitiTable :loading='loading' :dataSource='dataSource' :columns='columns' :change='change' :page-size='condition.page_size'  :total="total" @getSelection='getSelection'>
+					<!-- <div slot='table-btns' style="display:inline-block">
+						<Poptip
+							confirm
+							title="确定要删除吗?"
+							@on-ok='selectionDelete'
+							>
+							<div class='zmiti-table-btn'>删除</div>
+							
+						</Poptip>
+					</div> -->
+				</ZmitiTable>
+			</div>
+		</div>
 	</div>
 </template>
 
 <style lang="scss" scoped>
 	@import './index.scss';
 </style>
-<script>
 
+<script>
 	import Vue from 'vue';
 	import zmitiUtil from '../../../common/lib/util';
-	import ZmitiMask from '../../../common/mask';
-	import {timeRange} from '../../../common/config'
-	var {orderFoodActions,userActions} = zmitiUtil;
+	import ZmitiMask from '../../../common/mask/';
+	import ZmitiTable from '../../../common/table';
+	var {companyActions,zmitiActions,partyActions,formatDate} = zmitiUtil;
 
+	import {manuscriptStatus} from '../../../common/config';
 	export default {
 		props:['obserable'],
 		name:'zmitiindex',
 		data(){
 			return{
-				dataSource:{
-					columns:[
-						{
-							title:"时间",
-							width:200,
-							align:'center',
-							 fixed: 'left',
-							render:(h,params)=>{
-								return h('div',{},timeRange[params.index]);
-							}
-						},
-						{
-							title:'第一会议室',
-							width:200,
-							key:'meeting1',
-							align:'center',
-							render:(h,params)=>{
-								return h('div',{
-									style:{
-										width:'100%',
-										height:'30px',
-										background:params.row.meeting1? 'red':'transparent'
 
-									}
-								})
-							}
-						},
-						{
-							title:'第二会议室',
-							width:200,
-							key:'meeting2',
-							align:'center',
-							render:(h,params)=>{
-								return h('div',{
-									style:{
-										width:'100%',
-										height:'100%',
-										width:'100%',
-										height:'30px',
-										background:params.row.meeting2? 'red':'transparent'
+				
+				targetKeys:[],
+				showAvatarModal:false,
+				
+				companyname:zmitiUtil.getCurrentCompanyId().companyname,
+				roleList:[],
+				imgs:window.imgs,
+				isLoading:false,
+				showDetail:false,
+				total:0,
+				showDetailPage:-1,
+				currentClassId:-1, 
+				adminuserId:'',
+				loading:true,
+				currentUserid:'',
+				formObj:{
+					jobid:'',
+					transportid:''
+				},
+				address:'',
+				showPass:false,
+				showMap:false,
+				viewH:window.innerHeight,
+				viewW:window.innerWidth,
+				dataSource:[],
+				groupList:[],
+				companyList:[],
+				hideMenu:false,
+				unJoinedCompany:[],
+				columns:[
+					{
+						title:"编号",
+						key:'partyid',
+						align:'left',
+						width:68
+					},
+					{
+						title:"活动名称",
+						key:'partyname',
+						align:'left',
+					},
+					{
+						title:"活动描述",
+						key:'abstract',
+						align:'left',
+					},
+					{
+						title:"开始时间",
+						key:'begintime',
+						align:'left',
+				//		render:(h,params)=>{
+				//			return h('div',{},this.formatDate(params.row.begintime));
+				//		}
+					},
+					{
+						title:"结束时间",
+						key:'enteredtime',
+						align:'left',
+					//	render:(h,params)=>{
+					//		return h('div',{},params.row.createtime);
+					//	}
+					},
+					{
+						title:'操作',
+						key:'action',
+						align:'center',
+						width:200,
+						render:(h,params)=>{
 
-									}
-								})
-							}
-						},
-						{
-							title:'第3会议室',
-							width:200,
-							key:'meeting3',
-							align:'center',
-							render:(h,params)=>{
-								return h('div',{
-									style:{
-										width:'100%',
-										height:'30px',
-										background:params.row.meeting3? 'red':'transparent'
+							return h('div', [
+                                h('Poptip',{
+									props:{
+										confirm:true,
+										title:"确定要删除吗？",
+										placement:'left'
 
+									},
+									on:{
+										'on-ok':()=>{
+											this.delete(params.row.traffic);
+										},
+										
 									}
-								})
-							}
-						},
-						{
-							title:'第4会议室',
-							width:200,
-							key:'meeting4',
-							align:'center',
-							render:(h,params)=>{
-								return h('div',{
+								},[
+									h('span', {
+										props: {
+											type: 'error',
+											size: 'small'
+										},
+										style:{
+											cursor:'pointer',
+											color:'#06C'
+										},
+										on: {
+											click: () => {
+											}
+										}
+									}, '删除')
+								]),
+								h('span',{
 									style:{
-										width:'100%',
-										height:'30px',
-										background:params.row.meeting4? 'red':'transparent'
-
+										cursor:'pointer',
+										color:"rgb(0, 102, 204)",
+										marginLeft:'10px'
+									},
+									on:{
+										click:()=>{
+											this.formObj = params.row;
+											Vue.obserable.trigger({
+												type:'toggleMask',
+												data:true
+											})
+										}
 									}
-								})
-							}
-						},
-						{
-							title:'第5会议室',
-							width:200,
-							key:'meeting5',
-							align:'center',
-							render:(h,params)=>{
-								return h('div',{
-									style:{
-										width:'100%',
-										height:'30px',
-										background:params.row.meeting5? 'red':'transparent'
-
-									}
-								})
-							}
-						},
-						{
-							title:'第6会议室',
-							width:200,
-							key:'meeting6',
-							align:'center',
-							render:(h,params)=>{
-								return h('div',{
-									style:{
-										width:'100%',
-										height:'30px',
-										background:params.row.meeting6? 'red':'transparent'
-
-									}
-								})
-							}
+								},'详情')
+                            ]);
+							
+							 
 						}
-					],
-					data:[
-						{
-							meeting1:1,
-							meeting2:0,
-							meeting3:1,
-							meeting4:0,
-							meeting5:0,
-							meeting6:1,
-						},{
-							meeting1:1,
-							meeting2:0,
-							meeting3:0,
-							meeting4:1,
-							meeting5:0,
-							meeting6:1,
-						},
-						{
-							meeting1:1,
-							meeting2:0,
-							meeting3:1,
-							meeting4:0,
-							meeting5:0,
-							meeting6:1,
-						},
-						{
-							meeting1:1,
-							meeting2:0,
-							meeting3:1,
-							meeting4:0,
-							meeting5:0,
-							meeting6:1,
-						},
-						{
-							meeting1:1,
-							meeting2:0,
-							meeting3:1,
-							meeting4:0,
-							meeting5:1,
-							meeting6:1,
-						},
-						{
-							meeting1:1,
-							meeting2:0,
-							meeting3:1,
-							meeting4:0,
-							meeting5:1,
-							meeting6:1,
-						},
-						{
-							meeting1:1,
-							meeting2:0,
-							meeting3:1,
-							meeting4:0,
-							meeting5:1,
-							meeting6:1,
-						},
-						{
-							meeting1:1,
-							meeting2:0,
-							meeting3:1,
-							meeting4:0,
-							meeting5:1,
-							meeting6:1,
-						},
-					]
+					}
+				],
+				
+				
+				 
+				directoryList:{
+
 				},
 				condition:{
 					page_index:0,
-					page_size:200,
+					page_size:10,
 				},
+				transport:[],
+				tripost:[],
+				userinfo:{},
+				selectList:[]
 			}
 		},
 		components:{
-			ZmitiMask
+			ZmitiMask,
+			ZmitiTable
 		},
 
 		beforeCreate(){
-			
+
 		},
 		mounted(){
+			window.s = this;
+			var userinfo = zmitiUtil.getUserInfo();
+			if(!userinfo){
+				this.$router.push({path:'/login'})
 
-		 
- 
-			//
-			
+			}
+			this.getDataList();
+			//this.getJobData();
 		},
 
 		watch:{
+			
 			$route:{
 				handler(){
 					var productid = this.productid;
-					productid && this.$router.push({path:'/meeingroom/'+productid});
+					productid && this.$router.push({path:'/party/'+productid});
 				}
 			}
-			
 			
 		},
 		
 		methods:{
 
-			 
-			closeMaskPage(){
-				Vue.obserable.trigger({
-					type:'toggleMask',
-					data:false
-				})
-			},
-		    
-			
-
-
-			init(fn){
+			getJobData(){
 				var s = this;
+				s.loading = true;
+
 				var t = setInterval(() => {
+					var productid =  this.$route.params.id ;
 					if(Vue.productList){
 						clearInterval(t);
-						var productid =  this.$route.params.id ;
 						if(!productid){
+
+							Vue.productList.forEach(p=>{
+								if(s.$route.name.indexOf(p.producturl.substr(1))>-1){
+									productid  = p.productid;
+								}
+							})
+							
+						}
+						this.productid = productid;
+						var {condition} = this;
+						condition = Object.assign(condition,{
+							companyid:zmitiUtil.getCurrentCompanyId().companyid,
+							productid
+						})
+						zmitiUtil.ajax({
+							remark:"getJobList",
+							data:{
+								action:partyActions.getJobList.action,
+								condition:condition
+							},
+							success(data){
+								s.loading = false;
+								if(data.getret === 0){
+									s.total = data.total;
+									s.tripost = data.list;
+								}
+							}
+						})
+					}
+				}, 100);
+
+				
+			},
+			getbaseData(productid){
+				var s=this;
+
+				zmitiUtil.ajax({
+					remark:'getTrafficbase',
+					data:{
+						action:partyActions.getTrafficbase.action,
+						condition:{
+							companyid:zmitiUtil.getCurrentCompanyId().companyid,
+							productid
+						}
+					},
+					success(data){
+						if(data.getret === 0){
+							s.transport = data.list;
+						}
+					}
+				})
+			
+				
+			},
+			closeMaskPage(){
+				Vue.obserable.trigger({type:'toggleMask',data:false});
+			},
+			add(){
+				this.formObj = {};
+				Vue.obserable.trigger({type:'toggleMask',data:true});
+			},
+
+			adminAction(){
+				var s = this;
+				var action = this.formObj.traffic ? partyActions.editTraffic.action:partyActions.addTraffic.action;
+
+				let info = {
+
+					productid:s.productid,
+					jobid:this.formObj.jobid,
+					transportid:this.formObj.transportid,
+					companyid:zmitiUtil.getCurrentCompanyId().companyid
+				}
+				if(this.formObj.traffic){
+					info.traffic = this.formObj.traffic
+				}
+				zmitiUtil.ajax({
+					remark:this.formObj.traffic ?　'editTraffic':'addTraffic',
+					data:{
+						action,
+						info
+					},
+					success(data){
+						
+						s.$Message[data.getret === 0 ? 'success':'error'](data.msg||data.getmsg);
+						s.closeMaskPage();
+						if(data.getret === 0){
+							s.getDataList();
+						}
+					}
+				})
+			},
+			
+			change(e){
+				this.condition.page_index = e -1;
+				this.getDataList();
+			},
+		 
+			 
+
+			delete(traffic){
+				var s = this;
+				zmitiUtil.ajax({
+					remark:'delParty',
+					data:{
+						action:partyActions.delParty.action,
+						condition:{
+							traffic,
+							companyid:zmitiUtil.getCurrentCompanyId().companyid,
+							productid:s.productid
+						}
+					},
+					success(data){
+						s.$Message[data.getret === 0 ? 'success':'error'](data.msg||data.getmsg);
+						if(data.getret === 0){
+							
+							s.getDataList();
+							//s.dataSource = data.list;	 
+						}
+					}
+				})
+			},
+			change(e){
+				this.condition.page_index = e -1;
+				this.getDataList();
+			},
+			getDataList(){
+				var s = this;
+				s.loading = true;
+
+				var t = setInterval(() => {
+					var productid =  this.$route.params.id ;
+					if(Vue.productList){
+						clearInterval(t);
+
+						if(!productid){
+
 							Vue.productList.forEach(p=>{
 								if(s.$route.name.indexOf(p.producturl.substr(1))>-1){
 									productid  = p.productid;
 								}
 							})
 						}
-						this.$router.push({path:'/meeingroom/'+productid});
+						this.$router.push({path:'/party/'+productid});
 						this.productid = productid;
-						fn && fn(productid)
+					//	this.getbaseData(productid);
+						var {condition} = this;
+						condition = Object.assign(condition,{
+							companyid:zmitiUtil.getCurrentCompanyId().companyid,
+							productid
+						})
+						zmitiUtil.ajax({
+							remark:"getPartyList",
+							data:{
+								action:partyActions.getPartyList.action,
+								condition:condition
+							},
+							error(){
+								s.loading = false;
+							},
+							success(data){
+								s.loading = false;
+								if(data.getret === 0){
+									s.total = data.total;
+									s.dataSource = data.list;
+								}
+							}
+						})
 					}
 				}, 100);
+
+				
 			},
-
-		 
-
-		 
- 
-		 
-
+			getSelection(data){				
+				this.selectList = data;
+			},
+			selectionDelete(){
+				if(this.selectList.length<=0){
+					this.$Message.error({content:'您还未选择任何要删除的项',duration:5});
+					return;
+				}
+				console.log(this.selectList,'data-select')
+				var jobid = this.selectList.map(item=>{
+					return item.jobid;
+				}).join(',');
+				console.log(jobid,'jobid-jobid')
+				this.delete(jobid);
+			},
+			
 		}
 	}
 </script>
- 
